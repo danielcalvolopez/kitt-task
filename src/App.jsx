257cf59e581b5e518a989d7fe536e6887d291585
danielcalvolopez <acidistrict@gmail.com>
2022-12-13 18:36:12 +0000

    js task kitt

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TimeBooking from "./components/TimeBooking";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      <TimeBooking />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/utils/tariffs.js
@@ -0,0 +1,22 @@
+export const tariffs = {
+  minute: {
+    name: "Per minute",
+    pricePounds: 2,
+    priceDollars: 2.5,
+  },
+  hour: {
+    name: "Per hour",
+    pricePounds: 22,
+    priceDollars: 27,
+  },
+  day: {
+    name: "Per day",
+    pricePounds: 60,
+    priceDollars: 99,
+  },
+  week: {
+    name: "Per week",
+    pricePounds: 105,
+    priceDollars: 130,
+  },
+};
